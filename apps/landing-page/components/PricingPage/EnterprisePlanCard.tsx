5b24c119e3b913593d2aa25119ab54c4c60da8c4
Ryan <xu.ryan@outlook.com>
2023-12-16 16:32:02 +0800

    Update EnterprisePlanCard.tsx

--- apps/landing-page/components/PricingPage/EnterprisePlanCard.tsx
+++ apps/landing-page/components/PricingPage/EnterprisePlanCard.tsx
@@ -29,7 +29,7 @@ export const EnterprisePlanCard = () => (
       <Text fontSize="lg">
         <Button
           as={Link}
-          href="https://typebot.io/enterprise-lead-form"
+          href="https://bots.facto.com.cn/enterprise-lead-form"
           isExternal
           variant="outline"
         >
