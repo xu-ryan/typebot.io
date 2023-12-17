1a4cc570eef9b81056e989b4bbab7c8cfbe56683
Ryan <xu.ryan@outlook.com>
2023-12-17 18:23:56 +0800

    Update GithubLogo.tsx

--- apps/landing-page/assets/icons/GithubLogo.tsx
+++ apps/landing-page/assets/icons/GithubLogo.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 export const GitHubIcon = (props: IconProps) => (
   <Icon
     xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 24 24"
+    viewBox="0 0 48 48"
     fill="none"
     stroke="currentColor"
     strokeWidth="2"
