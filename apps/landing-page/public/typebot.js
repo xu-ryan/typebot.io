b3afede53dfa90cedafef38ed7894960e35d53e7
Ryan <xu.ryan@outlook.com>
2023-12-20 00:23:07 +0800

    Update typebot.js

--- apps/landing-page/public/typebot.js
+++ apps/landing-page/public/typebot.js
@@ -31,7 +31,7 @@ var  url=location.href;
     wx.ready(function () {
          //分享到朋友圈
      wx.updateTimelineShareData({
-       title: 'Typebot',
+       title: 'Typebot - Extremely flexible AI chatbot builder',
        link: url,
        imgUrl: 'https://bot.facto.com.cn/images/typebot.png',
        success: function (res) {
