6559bb9c28954c490675db642cd4beb59fa40b42
Ryan <xu.ryan@outlook.com>
2023-12-30 01:22:45 +0800

    Update tolgee.tsx

--- apps/builder/src/lib/tolgee.tsx
+++ apps/builder/src/lib/tolgee.tsx
@@ -1,13 +1,12 @@
 import { DevTools, Tolgee } from '@tolgee/react'
 import { FormatIcu } from '@tolgee/format-icu'
-import en from '../../public/locales/en.json'
-import cn from '../../public/locales/cn.json'
-import fr from '../../public/locales/fr.json'
-import de from '../../public/locales/de.json'
-import pt from '../../public/locales/pt.json'
-import ptBR from '../../public/locales/pt-BR.json'
-import es from '../../public/locales/es.json'
-import ro from '../../public/locales/ro.json'
+import en from '../i18n/en.json'
+import fr from '../i18n/fr.json'
+import de from '../i18n/de.json'
+import pt from '../i18n/pt.json'
+import ptBR from '../i18n/pt-BR.json'
+import es from '../i18n/es.json'
+import ro from '../i18n/ro.json'
 import { env } from '@typebot.io/env'
 
 export const tolgee = Tolgee()
@@ -17,11 +16,10 @@ export const tolgee = Tolgee()
     apiKey: env.NEXT_PUBLIC_TOLGEE_API_KEY,
     apiUrl: env.NEXT_PUBLIC_TOLGEE_API_URL,
     defaultLanguage: 'en',
-    availableLanguages: ['en', 'cn', 'fr', 'de', 'pt', 'pt-BR', 'es', 'ro'],
+    availableLanguages: ['en', 'fr', 'de', 'pt', 'pt-BR', 'es', 'ro'],
     fallbackLanguage: 'en',
     staticData: {
       en,
-      cn,
       fr,
       de,
       pt,
